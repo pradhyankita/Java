8da9086b0762855daaacc5d57d1160b15cd3765c
pradhyankita <104745890+pradhyankita@users.noreply.github.com>
2022-07-11 08:04:18 +0530

    Update pattern.java

--- src/com/BridgeLabz/Basics/pattern.java
+++ src/com/BridgeLabz/Basics/pattern.java
@@ -1,49 +1,41 @@
 package com.BridgeLabz.Basics;
 
-public class pattern {
-
-    public static void main(String[] args) {
-        int n=5;
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=i;j++){
-                System.out.print("*");
-            }
-            System.out.println();
-        }
-       int w=5;
-        for(int x=2;x<=w;x++){
-            for(int y=x;y<=w;y++){
-                System.out.print("*");
-            }
-            System.out.println();
-        }
-
-        System.out.println();
-        int q=1;
-        for (int r=3;r>=q;r++) {
-            for (int k = r; k >= q; k++) {
-                System.out.print(" ");
-            }
-            for (int k=1; k<r; k++) {
-                System.out.print("*");
-            }
-            for(int k=1;k<=r;k++){
-                System.out.print("*");
-            }
-            System.out.println();
-        }
-        int num=4;
-        for (int r=num;r>=1;r--) {
-            for (int k=r; k<num; k++) {
-                System.out.print(" ");
-            }
-            for (int k=1; k<=(2*r-1); k++) {
-                System.out.print("*");
-            }
-            //for(int k=1;k<=r;k++){
-              //  System.out.print("*");
-            }
-            System.out.println();
-        }
-    }
-
+import java.util.Scanner;  
+public class Pattern  
+{  
+public static void main(String args[])  
+{  
+int row, i, j, space = 1;  
+System.out.print("Enter the number of rows you want to print: ");  
+Scanner sc = new Scanner(System.in);  
+row = sc.nextInt();  
+space = row - 1;  
+for (j = 1; j<= row; j++)  
+{  
+for (i = 1; i<= space; i++)  
+{  
+System.out.print(" ");  
+}  
+space--;  
+for (i = 1; i <= 2 * j - 1; i++)  
+{  
+System.out.print("*");  
+}  
+System.out.println("");  
+}  
+space = 1;  
+for (j = 1; j<= row - 1; j++)  
+{  
+for (i = 1; i<= space; i++)  
+{  
+System.out.print(" ");  
+}  
+space++;  
+for (i = 1; i<= 2 * (row - j) - 1; i++)  
+{  
+System.out.print("*");  
+}  
+System.out.println("");  
+}  
+}  
+}  
